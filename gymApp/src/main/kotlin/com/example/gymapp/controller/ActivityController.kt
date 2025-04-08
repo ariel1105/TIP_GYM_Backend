7cf149929562c0a85aca74eddcad72928f373d48
ariel1105 <73146826+ariel1105@users.noreply.github.com>
2025-04-08 15:15:12 -0300

    Controller Activity getTurn

--- gymApp/src/main/kotlin/com/example/gymapp/controller/ActivityController.kt
+++ gymApp/src/main/kotlin/com/example/gymapp/controller/ActivityController.kt
@@ -1,9 +1,11 @@
 package com.example.gymapp.controller
 
 import com.example.gymapp.model.Activity
+import com.example.gymapp.model.Turn
 import com.example.gymapp.service.ActivityService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -15,5 +17,10 @@ class ActivityController {
     @GetMapping("/activities")
     fun getActivities(): List<Activity>{
         return activityService.getActivities()
+    }
+
+    @GetMapping("/turns/{activityId}")
+    fun getTurns(@PathVariable activityId: String): List<Turn>{
+        return return activityService.getTurnsActivity(activityId.toLong())
     }
 }
